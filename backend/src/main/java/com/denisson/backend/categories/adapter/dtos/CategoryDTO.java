7364da8b2b598fe83a253b387a869d48b32fb26a
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2024-09-29 12:12:41 -0300

    Add swagger

--- backend/src/main/java/com/denisson/backend/categories/adapter/dtos/CategoryDTO.java
+++ backend/src/main/java/com/denisson/backend/categories/adapter/dtos/CategoryDTO.java
@@ -1,9 +1,14 @@
 package com.denisson.backend.categories.adapter.dtos;
 
 import com.denisson.backend.abstracter.adapter.interfaces.AbstractDTOInterface;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Data Transfer Object (DTO) for representing a category for an establishment.")
 public record CategoryDTO(
+        @Schema(description = "The name of the category", example = "Awesome Pizza")
         String name,
+        @Schema(description = "The image URL representing the category, which may be used for display purposes in client applications.",
+                example = "https://example.com/images/category-pizza.png")
         String image
 ) implements AbstractDTOInterface {
     @Override
--- backend/src/main/java/com/denisson/backend/product/adapter/dtos/ProductDTO.java
+++ backend/src/main/java/com/denisson/backend/product/adapter/dtos/ProductDTO.java
@@ -1,14 +1,30 @@
 package com.denisson.backend.product.adapter.dtos;
 
 import com.denisson.backend.abstracter.adapter.interfaces.AbstractDTOInterface;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Data Transfer Object (DTO) for representing a product in the system.")
 public record ProductDTO(
+        @Schema(description = "The name of the product.", example = "Deluxe Burger")
         String name,
+
+        @Schema(description = "Customer evaluation or rating of the product.", example = "4.5",
+                minimum = "0", maximum = "5")
         String evaluation,
+
+        @Schema(description = "Detailed description of the product.", example = "A gourmet burger made with organic ingredients.")
         String description,
+
+        @Schema(description = "Price of the product.", example = "19.99", format = "decimal")
         String price,
+
+        @Schema(description = "Category the product belongs to.", example = "Fast Food")
         String category,
+
+        @Schema(description = "Establishment where the product is available.", example = "Downtown Diner")
         String establishment,
+
+        @Schema(description = "Image URL of the product.", example = "https://example.com/images/burger.png")
         String image
 ) implements AbstractDTOInterface {
     @Override
@@ -16,3 +32,4 @@ public record ProductDTO(
         return "Product";
     }
 }
+
